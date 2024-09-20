3e40275553d87a847cd8bb269f79e45d0402189d
sama-004 <70210929+Sama-004@users.noreply.github.com>
2024-09-20 22:43:43 +0530

    add: key press count add: left click count add: right click count

--- logger/main.go
+++ logger/main.go
@@ -6,10 +6,66 @@ import (
 	hook "github.com/robotn/gohook"
 )
 
-func main() {
+func LeftClickLogger() {
+	leftClickCount := 0
+	event := hook.Start()
+	defer hook.End()
+	leftClickChan := make(chan int)
+
+	go func() {
+		for ev := range event {
+			if ev.Kind == hook.MouseDown {
+				if ev.Button == hook.MouseMap["left"] {
+					leftClickChan <- 1
+				}
+			}
+		}
+	}()
+
+	for {
+		select {
+		case <-leftClickChan:
+			leftClickCount++
+			fmt.Printf("Left Click count: %d\n", leftClickCount)
+		}
+	}
+}
+
+func rightClickLogger() {
+	rightClickCount := 0
+
+	event := hook.Start()
+	defer hook.End()
+
+	rightClickChan := make(chan int)
+
+	go func() {
+		for ev := range event {
+			if ev.Kind == hook.MouseDown {
+				if ev.Button == hook.MouseMap["center"] {
+					rightClickChan <- 1
+				}
+			}
+		}
+		if hook.AddEvent("mright") {
+			rightClickChan <- 1
+		}
+	}()
+
+	for {
+		select {
+		case <-rightClickChan:
+			rightClickCount++
+			fmt.Printf("Right Click count: %d\n", rightClickCount)
+		}
+	}
+}
+
+func keypress() {
 	keyPress := 0
 	event := hook.Start()
 	defer hook.End()
+
 	keyPressChan := make(chan int)
 	go func() {
 		for ev := range event {
@@ -22,7 +78,13 @@ func main() {
 		select {
 		case <-keyPressChan:
 			keyPress++
-			fmt.Printf("Total keypresses: %d\n", keyPress)
+			fmt.Printf("Key press count: %d\n", keyPress)
 		}
 	}
+}
+
+func main() {
+	LeftClickLogger()
+	rightClickLogger()
+	keypress()
 }
