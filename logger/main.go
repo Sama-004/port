43fb06e98561cb641bbf365c4c4a9d50f41c41e6
sama-004 <70210929+Sama-004@users.noreply.github.com>
2024-09-26 11:34:38 +0530

    refactor: calculate the clicks every hour and log it

--- logger/main.go
+++ logger/main.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"time"
 
 	hook "github.com/robotn/gohook"
 )
 
-func LeftClickLogger() {
-	leftClickCount := 0
-	event := hook.Start()
-	defer hook.End()
+var leftClickCount, rightClickCount, keyPress int
+
+func logger(event chan hook.Event) {
 	leftClickChan := make(chan int)
+	rightClickChan := make(chan int)
+	keyPressChan := make(chan int)
 
 	go func() {
 		for ev := range event {
@@ -18,6 +21,14 @@ func LeftClickLogger() {
 				if ev.Button == hook.MouseMap["left"] {
 					leftClickChan <- 1
 				}
+				// something is wrong with this package it registers center as right click and does not work on right click
+				// TODO: try to move away from this package later
+				if ev.Button == hook.MouseMap["center"] {
+					rightClickChan <- 1
+				}
+			}
+			if ev.Kind == hook.KeyDown {
+				keyPressChan <- 1
 			}
 		}
 	}()
@@ -27,55 +38,9 @@ func LeftClickLogger() {
 		case <-leftClickChan:
 			leftClickCount++
 			fmt.Printf("Left Click count: %d\n", leftClickCount)
-		}
-	}
-}
-
-func rightClickLogger() {
-	rightClickCount := 0
-
-	event := hook.Start()
-	defer hook.End()
-
-	rightClickChan := make(chan int)
-
-	go func() {
-		for ev := range event {
-			if ev.Kind == hook.MouseDown {
-				if ev.Button == hook.MouseMap["center"] {
-					rightClickChan <- 1
-				}
-			}
-		}
-		if hook.AddEvent("mright") {
-			rightClickChan <- 1
-		}
-	}()
-
-	for {
-		select {
 		case <-rightClickChan:
 			rightClickCount++
-			fmt.Printf("Right Click count: %d\n", rightClickCount)
-		}
-	}
-}
-
-func keypress() {
-	keyPress := 0
-	event := hook.Start()
-	defer hook.End()
-
-	keyPressChan := make(chan int)
-	go func() {
-		for ev := range event {
-			if ev.Kind == hook.KeyDown {
-				keyPressChan <- 1
-			}
-		}
-	}()
-	for {
-		select {
+			fmt.Printf("Right click count:%d\n", rightClickCount)
 		case <-keyPressChan:
 			keyPress++
 			fmt.Printf("Key press count: %d\n", keyPress)
@@ -83,8 +48,31 @@ func keypress() {
 	}
 }
 
+func updateDb() {
+	//also need time here. Time should be date and time.
+	currentTime := time.Now().Format("2006-01-02 15:04")
+	log.Printf("Updating database with LeftClick: %d Rightclick: %d Keypress: %d Time: %s\n", leftClickCount, rightClickCount, keyPress, currentTime)
+	// db logic goes here
+	// what happens when failed to update the db??? store the count with time in array and try to write later??
+	//after db write reset the counts
+	leftClickCount = 0
+	rightClickCount = 0
+	keyPress = 0
+	fmt.Printf("Reset counts Leftclick: %d Rightclick: %d, keypress: %d\n", leftClickCount, rightClickCount, keyPress)
+}
+
 func main() {
-	LeftClickLogger()
-	rightClickLogger()
-	keypress()
+	log.Print("Starting logger")
+	eventChan := hook.Start()
+	defer hook.End()
+	go logger(eventChan)
+	//1*time.Hour here
+	ticker := time.NewTicker(5 * time.Second)
+	defer ticker.Stop()
+	for {
+		select {
+		case <-ticker.C:
+			updateDb()
+		}
+	}
 }
